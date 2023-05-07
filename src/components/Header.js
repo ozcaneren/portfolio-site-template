2a3e91c78726077320c053451d5cf0d857fbe860
eren ozcan <erenozcandev@gmail.com>
2023-05-07 17:03:36 +0300

    add responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import Switcher from './Switcher';
+import Switcher from "./Switcher";
 
-function Header() {
+export default function Header() {
   return (
-    <>
-      <div className="flex justify-around py-3 bg-white dark:bg-[#1f1f1f] dark:text-gray-300">
-        <h1 className="flex justify-center items-center font-bold text-2xl font-poppins">
-          loremipsum.dev
-        </h1>
-        <Switcher />
-      </div>
-    </>
+    <header className="header sticky top-0 bg-white shadow-xl flex items-center justify-center px-8 py-02 bg-gray-200 dark:bg-[#1f1f1f] dark:text-gray-300 z-10">
+      <nav className="nav font-semibold text-lg">
+          <ul className="flex items-center gap-12 md:gap-[39rem] font-poppins">
+              <li>
+                <h1 className="font-bold text-2xl">erenozcan.dev</h1>
+              </li>
+              <li className="p-4 border-b-2 dark:border-b-2 border-gray-500 dark:border-gray-200 border-opacity-0 dark:border-opacity-0 hover:border-opacity-100 dark:hover:border-opacity-100 hover:text-gray-500 dark:hover:text-gray-100 duration-200 cursor-pointer">
+                <Switcher />
+              </li>
+          </ul>
+      </nav>
+    </header>
   )
 }
-
-export default Header;
--- src/components/Info.js
+++ src/components/Info.js
@@ -2,18 +2,24 @@ import photo from '../img/photo.jpg'
 
 function Info() {
   return (
-    <div className="bg-slate-50 dark:bg-[#1d1d1d]">
-      <div className="flex justify-center items-center h-[94vh] gap-[10rem]">
-        <div className="flex flex-col">
-          <h1 className="text-4xl font-bold font-poppins dark:text-zinc-200">
-            Software Developer
-          </h1>
-          <p className="font-poppins font-medium pt-6 text-xl text-gray-600 dark:text-zinc-400">
-            Hi, I'm Lorem Ipsum.A passionate Software Developer. <br /> Based in Arch / Linux.
-          </p>
+    <div className="flex justify-around items-center top-0 h-auto w-full z-9999 p-[1.8rem] md:p-[1.7rem] bg-slate-50 dark:bg-[#1d1d1d]">
+      <section className="flex flex-col h-[86vh] md:h-[88vh] w-full">
+        <div className="flex items-center justify-center max-w-[107rem] mx-auto my-0 md:px-[4rem] md:w-full md:h-full">
+          <div className='flex justify-center items-center h-[70vh] md:h-[80vh]'>
+            <div className='flex flex-col-reverse items-center justify-center gap-[2rem] md:flex-row md:gap-[10rem]'>
+              <div className='flex flex-col max-w-[55rem]'>
+                <h1 className="text-4xl font-bold font-poppins md:my-[2rem] lg:leading-5 md:leading-2 dark:text-zinc-200 md:items-center">
+                  Software Developer
+                </h1>
+                <p className="font-poppins font-medium pt-6 leading-6 text-xl text-gray-600 dark:text-zinc-400">
+                  Hi, I'm Lorem Ipsum.<br/>A passionate Software Developer. <br /> Based in Arch / Linux.
+                </p>
+              </div>
+              <img src={photo} alt='img' className="bg-no-repeat bg-center bg-cover h-[200px] w-[200px] md:h-[300px] md:w-[300px] border-4 border-neutral-900 dark:border-neutral-100 bg-no-repeat bg-center bg-cover rounded-full" />
+            </div>
+          </div>
         </div>
-        <img src={photo} alt='img' className="h-[250px] w-[250px] border-4 border-neutral-900 dark:border-neutral-100 bg-no-repeat bg-center bg-cover rounded-full" />
-      </div>
+      </section>
     </div>
   )
 }
