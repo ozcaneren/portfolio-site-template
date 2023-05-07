754ef94f48e64a5f3f55bbcaf6ccced180fc950b
eren ozcan <erenozcandev@gmail.com>
2023-05-07 13:26:35 +0300

    change photo

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,3 +1,5 @@
+import photo from '../img/photo.jpg'
+
 function Info() {
   return (
     <div className="bg-slate-50 dark:bg-[#1d1d1d]">
@@ -10,7 +12,7 @@ function Info() {
             Hi, I'm Lorem Ipsum.A passionate Software Developer. <br /> Based in Arch / Linux.
           </p>
         </div>
-        <img src="https://storage.prompt-hunt.workers.dev/clfmmgx650001m907e27rphnf_2" className="h-[250px] w-[250px] border-4 border-neutral-900 dark:border-neutral-100 bg-no-repeat bg-center bg-cover rounded-full" />
+        <img src={photo} alt='img' className="h-[250px] w-[250px] border-4 border-neutral-900 dark:border-neutral-100 bg-no-repeat bg-center bg-cover rounded-full" />
       </div>
     </div>
   )
